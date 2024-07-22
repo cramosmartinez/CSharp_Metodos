a948cd43a961e20ec0fc772f54380260cc6c498a
Carlos Ramos <cramos-2018521@kinal.edu.gt>
2024-07-21 19:32:40 -0600

    Agregar archivos de proyecto.

---
+++ CSharp_Metodos/Program.cs
@@ -0,0 +1,18 @@
+﻿abstract class Motocicleta
+{
+    // Cualquiera puede llamar a este método.
+    public void EncenderMotor() { /* Declaraciones del método aquí */ }
+
+    // Solo las clases derivadas pueden llamar a este método.
+    protected void AñadirGasolina(int galones) { /* Declaraciones del método aquí */ }
+
+    // Las clases derivadas pueden sobrescribir la implementación de la clase base.
+    public virtual int Conducir(int millas, int velocidad)
+    {
+        /* Declaraciones del método aquí */
+        return 1;
+    }
+
+    // Las clases derivadas deben implementar este método.
+    public abstract double ObtenerVelocidadMáxima();
+}
